0584cceea61bf1da7c1fdd2cebf14e10093a7a04
Pushpendra Sharma <122333300+pushpendra010@users.noreply.github.com>
2024-01-26 16:05:34 +0530

    Add files via upload
    😊

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const model = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-model');
+const btnsShowModel = document.querySelectorAll('.show-modal');
+console.log(btnsShowModel);
+for (let i = 0; i < btnsShowModel.length; i++) {
+    console.log(btnsShowModel[i].textContent);
+}
